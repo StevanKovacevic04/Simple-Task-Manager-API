2fac7f9e4ee00e639f4cd9b2a66ed5af15b04a30
StevanKovacevic04 <kovacevicstevan@yahoo.com>
2025-04-12 11:26:18 +0200

    implemented repositories

---
+++ src/main/java/ch/ost/TaskManager/task_manager/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ch.ost.TaskManager.task_manager.repository;
+
+import ch.ost.TaskManager.task_manager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
