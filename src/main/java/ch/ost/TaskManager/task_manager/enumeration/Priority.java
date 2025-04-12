90410fb4c5b47275fa72e92548c657712d83bb55
StevanKovacevic04 <kovacevicstevan@yahoo.com>
2025-04-12 11:37:07 +0200

    fixed issue with enums

--- src/main/java/ch/ost/TaskManager/task_manager/enumeration/Priority.java
+++
@@ -1,5 +0,0 @@
-package ch.ost.TaskManager.task_manager.enumeration;
-
-public enum Priority {
-    LOW, MEDIUM, HIGH
-}
--- src/main/java/ch/ost/TaskManager/task_manager/enumeration/Status.java
+++
@@ -1,5 +0,0 @@
-package ch.ost.TaskManager.task_manager.enumeration;
-
-public enum Status {
-    PENDING, IN_PROGRESS, DONE
-}
