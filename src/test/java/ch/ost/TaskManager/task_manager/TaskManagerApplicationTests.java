b0e4097dce6c9938fd3c165b40ceb96e04a3395b
StevanKovacevic04 <kovacevicstevan@yahoo.com>
2025-04-12 10:47:37 +0200

    Initial Commit

---
+++ src/test/java/ch/ost/TaskManager/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ost.TaskManager.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
