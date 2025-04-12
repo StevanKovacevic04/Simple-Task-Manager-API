47a5d47d2cca20a8e8def7eb4e7d1fd2a76b3f41
StevanKovacevic04 <kovacevicstevan@yahoo.com>
2025-04-12 11:40:22 +0200

    implemented services

--- src/main/java/ch/ost/TaskManager/task_manager/repository/TaskRepository.java
+++ src/main/java/ch/ost/TaskManager/task_manager/repository/TaskRepository.java
@@ -1,6 +1,5 @@
 package ch.ost.TaskManager.task_manager.repository;
 
-import ch.ost.TaskManager.task_manager.enumeration.Status;
 import ch.ost.TaskManager.task_manager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +9,5 @@ import java.util.List;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByUserId(Long userId);
-    List<Task> findByStatus(Status status);
+    List<Task> findByStatus(Task.Status status);
 }
